00d39ea3b5532cc172a48852f5596dfb640868b4
AradheaRangga <aradhearangga36973@gmail.com>
2023-12-07 07:26:15 +0700

    enroll detailSubject

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,9 +13,9 @@ class DashboardController extends Controller
     public function index(){
         $user = User::all();
         $mahasiswa = Mahasiswa::all();
-        // $mahasiswa_id = Mahasiswa::where('id', auth()->user()->mahasiswa->id)->get()->first();
-        // $detailSubject = DetailSubject::where('mahasiswa_id', auth()->user()->mahasiswa->id)->get()->first();
-        $subject = Subject::all();
+        $mahasiswa_id = Mahasiswa::where('id', auth()->user()->mahasiswa->id)->get()->first();
+        $detailSubject = DetailSubject::where('mahasiswa_id', auth()->user()->mahasiswa->id);
+        $subject = Subject::where('id', $mahasiswa_id->subject_id);
         // $dataArray = array();
         // foreach($subject as $item){
         //     $getDataDetailSubjek = DetailSubject::where('mahasiswa_id', $mahasiswa_id->id)->where('subject_id', $item->id)->first();
@@ -30,6 +30,6 @@ class DashboardController extends Controller
             else
                 return view('dosen.dashboard', compact('user', 'mahasiswa'));
         }else
-            return view('mahasiswa.dashboard', compact( 'subject'));
+            return view('mahasiswa.dashboard', compact( 'subject', 'detailSubject', 'mahasiswa'));
     }
 }
--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -46,24 +46,28 @@
             </thead>
             <tbody>
                 @foreach ($subject as $data)
-                    <tr
-                        class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row"
-                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize">
-                            {{ $data->subject_name }}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ $data->dosen->user->name }}
-                        </td>
-                        <td>
-                            <form action="{{ route('ambil_kelas_action', $data->id) }}" method="post">
-                                @csrf
-                                <button type="submit"
-                                    class="font-medium text-red-600 dark:text-red-500 hover:underline">Ambil
-                                    Kelas</button>
-                            </form>
-                        </td>
-                    </tr>
+                    @foreach ($detailSubject as $detail)
+                        @if ($data->id != $detail->subject_id)
+                            <tr
+                                class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white capitalize">
+                                    {{ $data->subject_name }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $data->dosen->user->name }}
+                                </td>
+                                <td>
+                                    <form action="{{ route('ambil_kelas_action', $data->id) }}" method="post">
+                                        @csrf
+                                        <button type="submit"
+                                            class="font-medium text-red-600 dark:text-red-500 hover:underline">Ambil
+                                            Kelas</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endif
+                    @endforeach
                 @endforeach
             </tbody>
         </table>
